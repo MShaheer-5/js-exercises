03ba5a4a857201ee105d687b1d69b441c8a9c488
Muhammad Shaheer Hussain <muhammadshaheer5904@gmail.com>
2025-12-19 16:26:55 +0500

    Chp - 1: Learnt the usage of alert messages

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository includes several exercises in a chapter wise format that are intended to increase my javascript skills.
+
+All exercises were taken from repository of [Ghous Ahmed](https://github.com/ghousahmed/javascript-exercises).
---
+++ chapter-1/app.js
@@ -0,0 +1,15 @@
+//Q - 1
+alert("Hello visitor");
+
+//Q - 2
+alert("Error! Please enter a valid password");
+
+//Q - 3
+alert("Welcome to JS....\n Happy Coding!");
+
+//Q - 4
+alert("Welcome to JS....");
+alert("Happy Coding!");
+
+//Q-5
+alert("Hello... I can run JavaScript through my web browsers console.");
