56ed600fb6225ce8f84c0c6d4d0cbcba84f9abea
Muhammad Shaheer Hussain <muhammadshaheer5904@gmail.com>
2026-01-23 04:32:46 +0500

    Working on arrays

---
+++ chapter-14-16/app.js
@@ -0,0 +1,40 @@
+// Q - 1
+var students = [];
+
+// Q - 2
+var studentsObj = new Array();
+
+// Q - 3
+var fruits = ["Apple", "Banana", "Mango"];
+
+// Q - 4
+var numbers = [10, 20, 30, 40, 50];
+
+// Q - 5
+var flags = [true, false, true, false];
+
+// Q-6
+var mixedArray = ["Ali", 25, true, "Karachi"];
+
+//Q - 7
+var qualifications = [
+  "SSC",
+  "HSC",
+  "BCS",
+  "BS",
+  "BCOM",
+  "MS",
+  "M. Phil.",
+  "PhD",
+];
+
+console.log("<h2>Qualifications:</h2>");
+
+console.log("1) " + qualifications[0] + "<br>");
+console.log("2) " + qualifications[1] + "<br>");
+console.log("3) " + qualifications[2] + "<br>");
+console.log("4) " + qualifications[3] + "<br>");
+console.log("5) " + qualifications[4] + "<br>");
+console.log("6) " + qualifications[5] + "<br>");
+console.log("7) " + qualifications[6] + "<br>");
+console.log("8) " + qualifications[7] + "<br>");
