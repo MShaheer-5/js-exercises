34093a71f113aee7de6c6bd2c13337d7c4924742
Muhammad Shaheer Hussain <muhammadshaheer5904@gmail.com>
2026-01-23 01:10:02 +0500

    Understanding and using variables for strings

---
+++ chapter-2/app.js
@@ -0,0 +1,39 @@
+//Q - 1
+var username;
+
+//Q - 2
+var myName = "Muhammad Shaheer";
+
+//Q - 3
+var message;
+message = "Hello World";
+alert(message);
+
+//Q - 4
+var studentName = prompt("Enter student name: ");
+var studentAge = prompt("Enter student age: ");
+var studentCourse = prompt("Enter student course: ");
+
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+//Q - 5
+var food = "PIZZA\nPIZZ\nPIZ\nPI\nP\n";
+alert(food);
+
+//Q - 6
+var email = "abc123@email.com";
+alert("My email address is " + email);
+
+//Q - 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book" + book);
+
+//Q- 8
+const outputElement = document.getElementById("output");
+outputElement.textContent = "Yah! I can write HTML content through Javascript";
+
+//Q - 9
+var symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(symbol);
